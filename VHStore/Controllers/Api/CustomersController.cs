2ba4cdf0334179db584bddd96ef678ec8a363c0b
w3ll0n <wellon676@gmail.com>
2017-10-08 00:55:40 +0300

    Build an API for customers

---
+++ VHStore/Controllers/Api/CustomersController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Web.Http;
+using AutoMapper;
+using Microsoft.Ajax.Utilities;
+using VHStore.DTOs;
+using VHStore.Models;
+
+namespace VHStore.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //POST api/customer
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
+        }
+
+        //GET api/customers
+        public IEnumerable<CustomerDto> GetCustomers()
+        {
+            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+        }
+
+        //GET api/customer/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
+        }
+
+        //POST api/customer/1
+        [HttpPut]
+        public CustomerDto UpdateCustomer(int id, CustomerDto customerDto)
+        {
+
+            var customerFromDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerFromDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            Mapper.Map(customerDto, customerFromDb);
+            
+            _context.SaveChanges();
+
+            return Mapper.Map<Customer, CustomerDto>(customerFromDb);
+        }   
+
+        //DELETE api/customer/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ VHStore/DTOs/CustomerDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VHStore.Models;
+
+namespace VHStore.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please, enter customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        //[Min18YearsIsAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool IsSubcribedToNewsletter { get; set; }
+    }
+}
