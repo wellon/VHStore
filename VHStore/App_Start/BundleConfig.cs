95cc8b1494435952843b4d2a78f89da17196fc66
w3ll0n <wellon676@gmail.com>
2017-10-15 19:25:16 +0300

    Added Toastr notificator

--- VHStore/App_Start/BundleConfig.cs
+++ VHStore/App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace VHStore
                         "~/Scripts/respond.js",
                         "~/Scripts/DataTables/jquery.dataTables.js",
                         "~/Scripts/DataTables/dataTables.bootstrap.js",
-                        "~/Scripts/typeahead.bundle.js"
+                        "~/Scripts/typeahead.bundle.js",
+                        "~/Scripts/toastr.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -30,6 +31,7 @@ namespace VHStore
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/DataTables/css/dataTables.bootstrap.css",
                       "~/Content/typeahead.css",
+                      "~/Content/toastr.css",
                       "~/Content/site.css"));
         }
     }
